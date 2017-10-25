c102d47d4deb635f5fd3ae73e3a8bb8e25a8e8f3
Valmai <594837838@qq.com>
2017-10-25 11:18:11 +0800

    源码

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,15 @@
 #app .tab{
   display:flex;
   width:100%;
-  height:40px;
-  line-height:40px;
+  height:.4rem;
+  line-height:.4rem;
   border-bottom:1px solid rgba(7,17,27,0.1);
   .tab-item{
     flex:1;
     text-align:center;
-    font-size:28px;
-    color:#666;
     & > a{
       display:block;
-      font-size:14px;
+      font-size:.14rem;
       color:rgb(77,85,93);
       &.active{
         color:rgb(240,20,20);
