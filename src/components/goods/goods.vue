37d977c07a99e9f591f54fc5357876fdd3483af9
Valmai17 <594837838@qq.com>
2017-11-03 00:11:42 +0800

    源码

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,12 +1,102 @@
 <template>
-    <div class="header">
-    	我是goods
+    <div class="goods">
+        <div class="menu-wrapper">
+            <ul>
+                <li v-for="item in goods" class="menu-item">
+                    <span class="text  border-1px">
+                        <span v-show="item.type>0" class="icon" :class="classMap"></span>{{item.name}}
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <div class="foods-wrapper"></div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default{};
+  import axios from 'axios';
+  export default{
+    // props:{
+    //     seller:{
+    //         type:Object
+    //     }
+    // },
+    data(){
+        return{
+            goods:[]
+        }
+    },
+    created(){
+        this.classMap = ['decrease','discount','special','invoice','guarantee'];
+
+        axios.get('/api/goods')
+        .then(function (res) {
+            this.goods = res.data.data;
+            console.log(res.errno);
+            console.log(this.goods);
+        }.bind(this))
+        .catch(function (error) {
+            alert(error);
+        });
+    }
+  };
 </script>
 
-<style lang="less" rel="stylesheet/less">
+<style scoped  lang="less" rel="stylesheet/less">
+@import "../../common/less/mixin.less";
+.goods{
+    display:flex;
+    position:absolute;
+    top:1.74rem;
+    bottom:.46rem;
+    width: 100%;
+    overflow:hidden;
+    .menu-wrapper{
+        flex:0 0 .8rem;
+        width:.8rem;
+        background:#f3f5f7;
+        .menu-item{
+            display:table;
+            height:.54rem;
+            width:.56rem;
+            padding:0 .12rem;
+            line-height:.18rem;
+            .icon{
+                display:inline-block;
+                width:.12rem;
+                height:.12rem;
+                vertical-align:top;
+                margin-right:.02rem;
+                background-size:.12rem .12rem;
+                background-repeat:no-repeat;
+                &.decrease{
+                    .bg-image('decrease_3');
+                }
+                &.discount{
+                    .bg-image('discount_3');
+                }
+                &.guarantee{
+                    .bg-image('guarantee_3');
+                }
+                &.invoice{
+                    .bg-image('invoice_3');
+                }
+                &.special{
+                    .bg-image('special_3');
+                }
+            }
+            .text{
+                display:table-cell;
+                width:.56rem;
+                vertical-align:middle;
+                .border-1px(rgba(7,17,27,0.1));
+                font-size:.12rem;
+            }
+        }
+    }
+    .foods-wrapper{
+        flex:1;
+    }
+}
+
 </style>
