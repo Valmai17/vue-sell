b1915156a305e1812a9f3c7f13a43e5efb5f9260
Valmai <594837838@qq.com>
2017-11-02 16:27:29 +0800

    评分星星

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -4,9 +4,13 @@
     </div>
 </template>
 
-<script>//type="text/ecmascript-6"
+<script type="text/ecmascript-6">
+    const LENGTH = 5;
+    const CLS_ON = 'on';
+    const CLS_HALF = 'half';
+    const CLS_OFF = 'off';
     export default{
-        props: {
+        props: {//父子传数据
             size:{
                 type:Number
             },
@@ -14,9 +18,25 @@
                 type:Number
             }
         },
-        computed:{
-            starType{
+        computed:{ //计算属性
+            starType(){
                 return 'star-' + this.size;
+            },
+            itemClasses(){
+                let result = [];
+                let score = Math.floor(this.score*2)/2;
+                let hasDecimal = score % 1 !==0;
+                let integer = Math.floor(score);
+                for(let i=0;i<integer;i++){
+                    result.push(CLS_ON);
+                }
+                if(hasDecimal){
+                    result.push(CLS_HALF);
+                }
+                while(result.length < LENGTH){
+                    result.push(CLS_OFF);
+                }
+                return result;
             }
         }
     };
@@ -51,10 +71,44 @@
             }
         }
         &.star-36{
-            
+            .star-item{
+                width: .15rem;
+                height: .15rem;
+                margin-right: .06rem;
+                background-size: .15rem .15rem;
+                &:last-child{
+                    margin-right: 0;
+                }
+                &.on{
+                    .bg-image('star36_on');
+                }
+                &.half{
+                    .bg-image('star36_half');
+                }
+                &.off{
+                    .bg-image('star36_off');
+                }
+            }
         }
         &.star-24{
-            
+            .star-item{
+                width: .1rem;
+                height: .1rem;
+                margin-right: .03rem;
+                background-size: .1rem .1rem;
+                &:last-child{
+                    margin-right: 0;
+                }
+                &.on{
+                    .bg-image('star24_on');
+                }
+                &.half{
+                    .bg-image('star24_half');
+                }
+                &.off{
+                    .bg-image('star24_off');
+                }
+            }
         }
     }
 </style>
