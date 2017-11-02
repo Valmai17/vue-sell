127d23b55be161f33e043f74a009d90419e11aff
Valmai <594837838@qq.com>
2017-11-02 08:58:03 +0800

    源码

---
+++ src/components/star/star.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="star" :class="starType">
+    	<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+    </div>
+</template>
+
+<script>//type="text/ecmascript-6"
+    export default{
+        props: {
+            size:{
+                type:Number
+            },
+            score:{
+                type:Number
+            }
+        },
+        computed:{
+            starType{
+                return 'star-' + this.size;
+            }
+        }
+    };
+</script>
+
+<style  scoped lang="less" rel="stylesheet/less">
+    @import "../../common/less/mixin.less";
+    .star{
+        font-size: 0;
+        .star-item{
+            display: inline-block;
+            background-repeat: no-repeat;
+        }
+        &.star-48{
+            .star-item{
+                width: .2rem;
+                height: .2rem;
+                margin-right: .22rem;
+                background-size: .2rem .2rem;
+                &:last-child{
+                    margin-right: 0;
+                }
+                &.on{
+                    .bg-image('star48_on');
+                }
+                &.half{
+                    .bg-image('star48_half');
+                }
+                &.off{
+                    .bg-image('star48_off');
+                }
+            }
+        }
+        &.star-36{
+            
+        }
+        &.star-24{
+            
+        }
+    }
+</style>
