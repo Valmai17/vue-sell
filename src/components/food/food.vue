40146d2b392cd68e9c532c956aeadc7634329571
Valmai <594837838@qq.com>
2017-11-23 16:35:48 +0800

    源码

---
+++ src/components/food/food.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-show="showFlag" class="food">
+
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default{
+        props:{
+            food:{
+                type:Object
+            }
+        },
+        data(){
+            return {
+                showFlag: false
+            }
+        },
+        methods: {
+            show() {
+                this.showFlag = true;
+            }
+        }
+    }
+</script>
+<style lang="less">
+    .food{
+        position: fixed;
+        top:0;
+        left: 0;
+        bottom:.48rem;
+        z-index: 30;
+        width: 100%;
+        background: #fff;
+    }
+</style>
