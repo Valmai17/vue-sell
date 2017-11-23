8c6962c2f8709f34c9c9dedce4e6a69d6e9023a2
Valmai <594837838@qq.com>
2017-11-23 17:11:50 +0800

    源码

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,18 +1,31 @@
 <template>
     <transition name="move">
-        <div v-show="showFlag" class="food">
-        <div class="food-content">
-            <div class="image-header">
-                <img :src="food.image"/>
-                <div class="back" @click="hide">
-                    <i class="icon-arrow_lift"></i>
+        <div v-show="showFlag" class="food" ref="food">
+            <div class="food-content">
+                <!-- 食物图片 -->
+                <div class="image-header">
+                    <img :src="food.image"/>
+                    <!-- 返回按钮 -->
+                    <div class="back" @click="hide">
+                        <i class="icon-arrow_lift"></i>
+                    </div>
+                </div>
+                <div class="content">
+                    <h1 class="title">{{food.name}}</h1>
+                    <div class="detail">
+                        <span class="sell-count">月售{{food.sellCount}}</span><span class="rating">好评率{{food.rating}}</span>
+                    </div>
+                    <div class="price">
+                        <span class="now">￥{{food.price}}</span>
+                        <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     </transition>
 </template>
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
     export default{
         props:{
             food:{
@@ -27,6 +40,15 @@
         methods: {
             show() {
                 this.showFlag = true;
+                this.$nextTick(()=>{
+                    if(!this.scroll){
+                        this.scroll = new BScroll(this.$refs.food,{
+                            click:true
+                        })
+                    }else{
+                        this.scroll.refresh();
+                    }
+                });
             },
             hide(){
                 this.showFlag = false;
@@ -71,6 +93,43 @@
                     padding:.1rem;
                     font-size:.2rem;
                     color:#fff;
+                }
+            }
+        }
+        .content{
+            padding:.18rem;
+            .title{
+                line-height: .18rem;
+                margin-bottom:.08rem;
+                font-size:.18rem;
+                font-weight:700;
+                color:rgb(7,17,27);
+            }
+            .detail{
+                margin-bottom:.18rem;
+                height:.1rem;
+                line-height:.1rem;
+                font-size:0;
+                .sell-count,.rating{
+                    font-size:.14rem;
+                    color:rgb(147,153,159);
+                }
+                .sell-count{
+                    margin-right:.12rem;
+                }
+            }
+            .price{
+                font-weight:700;
+                line-height:.24rem;
+                .now{
+                    margin-right:.18rem;
+                    font-size:.18rem;
+                    color:rgb(240,20,20);
+                }
+                .old{
+                    text-decoration: line-through;
+                    font-size:.14rem;
+                    color:rgb(147,153,159);
                 }
             }
         }
